1271f6dd0d3ba03d9c39ecb59777754811ec69bc
Mariemzd <mariem.zid99@gmail.com>
2026-05-06 13:23:34 -0400

    little cleanup

--- MATLAB/simulations/compareTimes_RLvsForagingByRichnessog.m
+++ MATLAB/simulations/compareTimes_RLvsForagingByRichnessog.m
@@ -1,6 +1,7 @@
-%% how do we re-load the data now so we can work with it?
-
-cd('/Users/becket/Documents/MATLAB/RLvsForagingByRichness/simulationOutputs')
+clear ; close all ; 
+ogpath = '/Users/mac/Documents/python/ForagingByRichness/Decision_making_models/MATLAB/simulations/' ;
+cd(ogpath)
+cd('./SimulationsOutputs')
 
 files = dir;
 fnames = {files(find(~cellfun(@isempty,strfind({files.name},'simResults')))).name};
@@ -62,18 +63,19 @@ nanmean(theta,1)
 nanstd(theta,1)
 
 
-%% now pull in the human data
-
+%% now pull in the human data 
+close all ; 
 maxNComp = 2; % for the mixture models
 
 if ~exist('trials')
-    cd('/Users/becket/Documents/MATLAB/RLvsForagingByRichness')
-    load('singleiti_202203011023_lightweight.mat')
+    cd(ogpath)
+    load('/Users/mac/Documents/python/ForagingByRichness/Decision_making_models/data/singleiti_202203011023_lightweight.mat')
 end
 
 % and do the same here
 times = []; p_rwd = []; p_best = []; switches = [];
 
+% figure 4.e-f
 for k = 1:length(trials)
     % are we looking at non-practice trials and excluding the 4 people?
     selex = [trials(k).trials.practice]==0; % yep
@@ -93,7 +95,7 @@ for k = 1:length(trials)
     end
 end
 
-figure('Position',[449  138  1061  472]);
+figure('Position',[449  138  1061  472]); %figure 4 e-f
 
 subplot(1,6,1:3);
 twoMixPlot(times,15)
@@ -125,14 +127,6 @@ df2 = 3;
 df = df2 - df1;
 pAdd2 = 1-chi2cdf(llr,df)
 
-% subplot(1,6,5:6)
-% xeval = [10,15,20,50];
-% bar(arrayfun(@(x) nanmean(times>x),xeval));
-% ylim([0 0.25]);
-% set(gca,'FontSize',16,'XTick',[1:length(xeval)],...
-%     'XTickLabel',num2str(xeval'));
-% ylabel('p(run > length)')
-% xlabel('length')
 
 realTheta = exp2mix(times);
 realTheta(1:2) = realTheta(1:2); % convert to switch p
@@ -150,10 +144,12 @@ pltStr = {'short t constant','long t constant','% short',...
     'mean run length','p(switch)','mean runs > 30','mean runs > 60',...
     'p(reward)','p(chose best)'};
 
+%Figure 4.a-b/g-j
+
 figure('Position',[476   148   825   718]); hold on;
 for k = 1:size(theta,2)
     subplot(3,3,k); hold on;
-    violinPlot(squeeze(theta(:,k,:)))
+    violinPlot(squeeze(theta(:,k,:))) ; hold on ;
     pH = plot([1,2],mean(squeeze(theta(:,k,:))),'.k','MarkerSize',30);
     % 70% CIs
     eH = errorbar([1,2],mean(squeeze(theta(:,k,:))),...
@@ -176,7 +172,7 @@ for k = 1:size(theta,2)
     set(gca,'FontSize',14,'XTick',[1,2],...
         'XTickLabel',{'RL','FOR'})
     ylabel(pltStr{k});
-    xlabel('model');
+    xlabel('model');  hold on 
 
     pltStr{k}
     mean(squeeze(theta(:,k,:)))
@@ -187,7 +183,7 @@ for k = 1:size(theta,2)
 end
 
 %% does foraging do more of those 300 trial same choices?
-
+% figure 4.k
 for model = 1:2
     switch model
         case 1
@@ -291,6 +287,8 @@ end
 
 %%
 
+%Figure 4.c-d
+
 behOI = pSwitch;
 
 mdlStr = {'rl','foraging'};
@@ -333,188 +331,3 @@ nanmean((tmp(:,3)-tmp(:,1)))
 nanstd((tmp(:,3)-tmp(:,1)))
 [h,p,ci,stat] = ttest((tmp(:,3)-tmp(:,1)))
 
-%%
-
-nParams = 4;
-nBoot = size(out,2);
-theta = NaN(nBoot,nParams,2); % preallocate
-
-for model = 1:2
-    switch model
-        case 1
-            indx = find(strcmp({out(:,1).type},'rl'));
-        case 2
-            indx = find(strcmp({out(:,1).type},'f'));
-    end
-
-    for boot = 1:nBoot
-        % first, we have to reshape the data for our HMM code
-        choices = {out(indx,boot).choices};
-        rewards = {out(indx,boot).rewards}; % gotta reformat
-        data = arrayfun(@(k) {choices{k};rewards{k}},1:length(choices),'UniformOutput',false);
-
-       
-        data = data(arrayfun(@(k) length(unique(data{k}{1,:}))>1,1:length(data)));
-
-        % then run the HMM code and extract our parameters
-        [ll, transmat, ~,~,emat] = fitOreOitHMM(data',false); % fit
-        theta(boot,1,model) = transmat(1,1); % ore to ore
-        theta(boot,2,model) = transmat(2,2); % oit to oit
-
-        % now we can mess with the transition matrix
-        tmp = stationaryDist(transmat);
-        theta(boot,3,model) = tmp(1); % stability of ore
-        theta(boot,4,model) = tmp(2); % stability of oit
-
-    end
-end
-
-%% now same on the real peoples
-
-realTheta = NaN(1,nParams);
-data = cell(1,length(trials));
-for k = 1:length(trials)
-    selex = [trials(k).trials.practice]==0; % grab only practice
-    choices = [trials(k).trials(selex).choice]+1; % pull out choices
-    rewards = [trials(k).trials(selex).reward]; % pull out rewards;
-    if length(unique(choices))>1
-        data{k} = {choices;rewards};
-    end
-end
-
-data = {data{arrayfun(@(k) ~isempty(data{k}),1:length(data))}};
-
-% then run the HMM code and extract our parameters
-[ll, transmat, ~,~,emat] = fitOreOitHMM(data',false); % fit
-realTheta(1) = transmat(1,1); % ore to ore
-realTheta(2) = transmat(2,2); % oit to oit
-
-% now we can mess with the transition matrix
-tmp = stationaryDist(transmat);
-realTheta(3) = tmp(1); % stability of ore
-realTheta(4) = tmp(2); % stability of oit
-
-%% or we could just load the data b/c the above takes forever
-
-load('HMMdynamics_compareTimes.mat')
-
-%% multiply oit x 2 because there are 2 states
-
-theta(:,4,:) = theta(:,4,:)*2
-realTheta(4) = realTheta(4)*2
-
-%% and now the plots
-
-pltStr = {'ore2ore','oit2oit','stationary p(ore)','stationary p(oit)'};
-
-figure('Position',[476   148   825   718]); hold on;
-for k = 1:size(theta,2)
-    subplot(3,3,k); hold on;
-    violinPlot(squeeze(theta(:,k,:)))
-    pH = plot([1,2],mean(squeeze(theta(:,k,:))),'.k','MarkerSize',30);
-    % 70% CIs
-    eH = errorbar([1,2],mean(squeeze(theta(:,k,:))),...
-        mean(squeeze(theta(:,k,:)))-quantile(squeeze(theta(:,k,:)),[0.15]),...
-        mean(squeeze(theta(:,k,:)))-quantile(squeeze(theta(:,k,:)),[0.85]));
-    set(eH,'CapSize',0,'LineStyle','none','Color','k',...
-        'LineWidth',3);
-    % 95% CIs
-    eH = errorbar([1,2],mean(squeeze(theta(:,k,:))),...
-        mean(squeeze(theta(:,k,:)))-quantile(squeeze(theta(:,k,:)),[0.025]),...
-        mean(squeeze(theta(:,k,:)))-quantile(squeeze(theta(:,k,:)),[0.975]));
-    set(eH,'CapSize',0,'LineStyle','none','Color','k',...
-        'LineWidth',0.5);
-    line([0 3],[realTheta(k),realTheta(k)]);
-
-    set(gca,'FontSize',14,'XTick',[1,2],...
-        'XTickLabel',{'RL','FOR'})
-    ylabel(pltStr{k});
-    xlabel('model');
-
-    pltStr{k}
-    mean(squeeze(theta(:,k,:)))
-    quantile(squeeze(theta(:,k,:)),[0.025,0.975])
-
-    realTheta(k)
-    nanmean(realTheta(k)>squeeze(theta(:,k,:)))
-end
-
-%% plot the potentials
-
-figure(); hold all;
-for plt = 1:3
-    switch plt
-        case 1
-            tmp = realTheta;
-        case 2
-            tmp = geomean(theta(:,:,1)); % rl
-        case 3
-            tmp = geomean(theta(:,:,2)); % for
-    end
-
-    % we'll always illustrate it as oit, ore, so rectify inputs
-    twoWellPotential_v2([1-tmp(3),tmp(3)],[1-tmp(2),1-tmp(1)])
-    % set A to 0.18 in twoWellPotential_v2 for the nice illustration
-
-%     threeWellPotential([1-tmp(3),tmp(3)],[1-tmp(2),1-tmp(1)])
-end
-
-set(gca,'FontSize',14,'XTick',[1,2],'XTickLabel',{'oit','ore'},...
-    'YTick',[]);
-ylabel('potential energy')
-legend('participants','rl','foraging')
-wonk = ylim;
-ylim([wonk(1)-0.2, wonk(2)+0.3])
-
-
-%% let's look at the times, maybe compare the stickiness of the real data against our mass of simulations
-
-maxNComp = 2;
-
-figure('Position',[476   330   604   536]);
-
-for model = 1:2
-    switch model
-        case 1
-            selex = strcmp({out.type},'rl');
-            tStr = 'rl'
-            ax1pos = [1:3];
-            ax2pos = [5:6];
-        case 2
-            selex = strcmp({out.type},'f');
-            tStr = 'foraging'
-            ax1pos = [7:9];
-            ax2pos = [11:12];
-    end
-
-    times = [[out(selex).times]-1]; % do the minus up front
-
-    subplot(2,6,ax1pos);
-    twoMixLogPlot(times)
-    title(tStr)
-    ylim([10.^-4, 10^0])
-
-    % now calculate the log likelihood and add that to the plot
-    loglik = NaN(1,maxNComp);
-    try
-        for k = 1:maxNComp
-            [~,~,loglik(k)] = discreteExpMix(times,k);
-        end
-    end
-    
-    subplot(2,6,ax2pos);
-    plot([1:maxNComp],loglik,'.-k','MarkerSize',20)
-    set(gca,'FontSize',16,'XTick',[1:maxNComp]);
-    xlim([0 maxNComp+1]); %ylim([-3.5*10^4, -3*10^4])
-    ylabel('log likelihood')
-    xlabel('n components')
-    
-    length(times)
-    loglik
-    llr = -2*(loglik(1) - loglik(2))
-    df1 = 1;
-    df2 = 3;
-    df = df2 - df1;
-    pAdd2 = 1-chi2cdf(llr,df)
-
-end
